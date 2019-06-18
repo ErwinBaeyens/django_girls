bd7cc9c22b0ce410f458bb5e7e759d900e651485
Erwin Baeyens <Erwin.Baeyens@ypto.be>
2019-06-18 11:37:09 +0200

    Modified templates to show posts from database

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
+
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
+def post_list(request):
+    me = User.objects.get(username='ebaeyens')
+    posts = Post.objects.filter(author=me).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+
